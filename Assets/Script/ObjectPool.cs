53b25fc248ecd7ec50fd3d85fe80c38166af3b0e
Guilherme Cardoso <gcardoso@duck.com>
2024-01-30 19:23:07 +1300

    Object Pools

--- Assets/Script/ObjectPool.cs
+++ Assets/Script/ObjectPool.cs
@@ -7,6 +7,24 @@ public class ObjectPool : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private float spawnTimer = 1f;
+    [SerializeField] private int poolSize = 5;
+
+    GameObject[] pool;
+
+    private void Awake() {
+        PopulatePool();
+    }
+
+    private void PopulatePool()
+    {
+        pool = new GameObject[poolSize];
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            pool[i] = Instantiate(enemyPrefab, transform);
+            pool[i].SetActive(false);
+        }
+    }
 
     void Start()
     {
@@ -17,8 +35,20 @@ public class ObjectPool : MonoBehaviour
     {
         while (true)
         {
-            Instantiate(enemyPrefab, transform);
+            EnableObjectInPool();
             yield return new WaitForSeconds(spawnTimer);
         };
+    }
+
+    private void EnableObjectInPool()
+    {
+        foreach (var enemy in pool)
+        {
+            if (!enemy.activeInHierarchy)
+            {
+                enemy.SetActive(true);
+                return;
+            }
+        }
     }
 }
