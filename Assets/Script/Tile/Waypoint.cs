69f626f2db625590487b46dd84048a7620e9ae92
Guilherme Cardoso <gcardoso@duck.com>
2024-02-06 20:37:05 +1300

    Currency System (Part 2)

--- Assets/Script/Tile/Waypoint.cs
+++ Assets/Script/Tile/Waypoint.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    [SerializeField] GameObject towerPrefab;
+    [SerializeField] Tower towerPrefab;
     [SerializeField] bool isPlaceble;
     public bool IsPlaceble { get { return isPlaceble;} }
 
     private void OnMouseDown() {
         if (isPlaceble)
         {
-            Instantiate(towerPrefab, transform.position, Quaternion.identity);
-            isPlaceble = false;
+            var wasTowerPlaced = towerPrefab.CreateTower(towerPrefab, transform.position);
+            isPlaceble = !wasTowerPlaced;
         }
     }
 }
