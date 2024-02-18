94b35631ba8771a1df5bb05cee8589208fa9ff98
Guilherme Cardoso <gcardoso@duck.com>
2024-02-18 18:18:21 +1300

    Increasing Difficulty

--- Assets/Script/Enemy/EnemyHealth.cs
+++ Assets/Script/Enemy/EnemyHealth.cs
@@ -2,9 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     [SerializeField] int maxHitPoints = 5;
+
+    [Tooltip("Adds amount to maxHitPoints when enemy dies.")]
+    [SerializeField] int difficultyRamp = 1;
+
     int currentHitPoints = 0;
     Enemy enemy;
 
@@ -12,7 +17,6 @@ public class EnemyHealth : MonoBehaviour
         enemy = GetComponent<Enemy>();
     }
 
-    // Start is called before the first frame update
     void OnEnable()
     {
         currentHitPoints = maxHitPoints;
@@ -31,6 +35,7 @@ public class EnemyHealth : MonoBehaviour
         {
             enemy.RewardGold(); 
             gameObject.SetActive(false);
+            maxHitPoints += difficultyRamp;
         }
     }
 }
