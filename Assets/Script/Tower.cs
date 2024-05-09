f60c2eec7e0ba9484cc86376c9d05e08bab9946c
Guilherme Cardoso <gcardoso@duck.com>
2024-05-09 20:13:09 +1200

    Build Timer

--- Assets/Script/Tower.cs
+++ Assets/Script/Tower.cs
@@ -6,10 +6,39 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] int cost = 75;
+    [SerializeField] float buildDelay = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(Build());
+    }
+
+    private IEnumerator Build()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(false);
+            }
+        }
+
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+            yield return new WaitForSeconds(buildDelay);
+            foreach (Transform grandchild in child)
+            {
+                grandchild.gameObject.SetActive(true);
+            }
+        }
+    }
 
     public bool CreateTower(Tower towerPrefab, Vector3 position)
     {
-        Bank bank = FindObjectOfType<Bank>(); 
+        Bank bank = FindObjectOfType<Bank>();
         if (bank == null)
         {
             return false;
